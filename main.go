59af2d5c1089467b126b60ce40c980f9601678c2
Work User <work@company.com>
2025-03-18 11:33:00 +0000

    add Switch User func

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/surbytes/gitusr/utils"
 )
 
 func main() {
+	if len(os.Args) > 2 && os.Args[1] == "switch" {
+		err := utils.SwitchUserByName(os.Args[2])
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+			os.Exit(1)
+		}
+	} else {
 
-	utils.RenderUsers()
-
+		utils.RenderUsers()
+	}
 }
